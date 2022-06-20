7abd4f2d240be13bc83ee79e1e42495fc9c5c01e
NwaguChima <nwaguchima48@gmail.com>
2022-06-20 22:59:48 +0100

    rendered logo image

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <Image
+        alt="Bird App"
+        width="50"
+        height="50"
+        src="https://www.svgrepo.com/show/88851/cute-bird.svg"
+      ></Image>
+    </div>
+  );
 };
 
 export default Sidebar;
