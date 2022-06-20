caeee1a2d28822f72a7499441b573e1ff96dbbc0
NwaguChima <nwaguchima48@gmail.com>
2022-06-20 23:17:41 +0100

    rendered multiple sidebar items

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import SidebarMenuItem from "./SidebarMenuItem";
-import { HomeIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
+  InboxIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Sidebar = () => {
   return (
@@ -18,6 +27,13 @@ const Sidebar = () => {
       {/* <SidebarMenuItem /> */}
       <div>
         <SidebarMenuItem text="Home" Icon={HomeIcon} />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="List" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
     </div>
   );
