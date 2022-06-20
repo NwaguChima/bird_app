ef2a4efa1e46147eddd49002b19ca88e27efa28b
NwaguChima <nwaguchima48@gmail.com>
2022-06-20 23:02:29 +0100

    setup sidebar menu items

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import SidebarMenuItem from "./SidebarMenuItem";
 
 const Sidebar = () => {
   return (
     <div>
-      <Image
-        alt="Bird App"
-        width="50"
-        height="50"
-        src="https://www.svgrepo.com/show/88851/cute-bird.svg"
-      ></Image>
+      <div>
+        <Image
+          alt="Bird App"
+          width="50"
+          height="50"
+          src="https://www.svgrepo.com/show/88851/cute-bird.svg"
+        ></Image>
+      </div>
+
+      {/* <SidebarMenuItem /> */}
+      <div>
+        <SidebarMenuItem />
+      </div>
     </div>
   );
 };
