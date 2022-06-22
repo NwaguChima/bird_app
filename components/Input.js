d68c1eac171ae4286817f3570e1cae4e88d41900
NwaguChima <nwaguchima48@gmail.com>
2022-06-22 02:41:22 +0100

    completed the input section of the feed component

--- components/Input.js
+++ components/Input.js
@@ -4,7 +4,7 @@ import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
 
 const Input = () => {
   return (
-    <div className="flex border-b border-gray-200 p-3 space-x-3">
+    <div className="flex items-start border-b border-gray-200 p-3 space-x-3">
       <Image
         src="https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1"
         alt="user image"
@@ -18,14 +18,16 @@ const Input = () => {
             className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
             rows="2"
             placeholder="What's happening"
-          />
+          ></textarea>
         </div>
-        <div>
-          <div>
-            <PhotographIcon className="h-10 w-10" />
-            <EmojiHappyIcon className="h-10 w-10" />
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex">
+            <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+            <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
           </div>
-          <button>Tweet</button>
+          <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+            Tweet
+          </button>
         </div>
       </div>
     </div>
