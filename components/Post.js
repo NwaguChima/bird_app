0645b8bbc7983e2d5d7e87f9b3454bbc5b872975
NwaguChima <nwaguchima48@gmail.com>
2022-06-23 00:08:51 +0100

    setup the widget component

--- components/Post.js
+++ components/Post.js
@@ -12,7 +12,6 @@ import React from "react";
 const Post = ({ post }) => {
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200 items-start">
-      {/* user image */}
       <Image
         src={post.userImg}
         alt="user-badge"
@@ -21,9 +20,7 @@ const Post = ({ post }) => {
         className="h-11 w-11 rounded-full mr-4"
       />
 
-      {/* right side */}
       <div>
-        {/* Header */}
         <div className="flex items-center justify-between">
           {/* post user info */}
           <div className="flex space-x-1 items-center whitespace-nowrap">
@@ -35,15 +32,12 @@ const Post = ({ post }) => {
               {post.timestamp}
             </span>
           </div>
-          {/* icon */}
           <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
         </div>
-        {/* post content */}
         <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
           {post.text}
         </p>
 
-        {/* post image */}
         <Image
           src={post.img}
           alt="post-image"
@@ -52,7 +46,6 @@ const Post = ({ post }) => {
           className="rounded-2xl mr-2"
         />
 
-        {/* post icons */}
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
