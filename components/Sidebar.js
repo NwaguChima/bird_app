fd8f5e9595e03b4b7dc7a076a5c71deca57389fd
NwaguChima <nwaguchima48@gmail.com>
2022-06-22 01:41:37 +0100

    completed the sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -16,7 +16,7 @@ import {
 const Sidebar = () => {
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
-      <div className="hoverEffect">
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image
           alt="Bird App"
           width="50"
@@ -26,7 +26,7 @@ const Sidebar = () => {
       </div>
 
       {/* <SidebarMenuItem /> */}
-      <div>
+      <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
         <SidebarMenuItem text="Notifications" Icon={BellIcon} />
