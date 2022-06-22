3cf86d028b8db1c0e2dbf0fe207c8746d868789d
NwaguChima <nwaguchima48@gmail.com>
2022-06-22 02:29:24 +0100

    styled input and image div

--- components/Input.js
+++ components/Input.js
@@ -4,16 +4,21 @@ import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
 
 const Input = () => {
   return (
-    <div>
+    <div className="flex border-b border-gray-200 p-3 space-x-3">
       <Image
         src="https://i0.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1"
         alt="user image"
         width="50"
         height="50"
+        className="h-11 w-11 rounded-full curpus-pointer hover:brightness-95"
       />
-      <div>
-        <div>
-          <textarea rows="2" placeholder="What's happening"></textarea>
+      <div className="w-full divide-y divide-gray-200">
+        <div className="">
+          <textarea
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+            rows="2"
+            placeholder="What's happening"
+          />
         </div>
         <div>
           <div>
