f81f1d7c8dd80011502ee26a253d0efadeb4edd1
NwaguChima <nwaguchima48@gmail.com>
2022-06-23 01:21:10 +0100

    completed the news feed widget

--- components/Widgets.js
+++ components/Widgets.js
@@ -3,6 +3,8 @@ import React from "react";
 import News from "./News";
 
 const Widgets = ({ newsResult }) => {
+  const [articleNum, setArticleNum] = React.useState(3);
+
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py1.5 z-50">
@@ -17,10 +19,13 @@ const Widgets = ({ newsResult }) => {
       </div>
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">What&apos;s hapenning</h4>
-        {newsResult.map((article) => (
+        {newsResult.slice(0, articleNum).map((article) => (
           <News key={article.title} article={article} />
         ))}
-        <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+        <button
+          onClick={() => setArticleNum(articleNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
           Show more
         </button>
       </div>
