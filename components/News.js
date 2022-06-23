458d560cad11b64fb86be33f994a4962b4980409
NwaguChima <nwaguchima48@gmail.com>
2022-06-23 01:17:21 +0100

    completed ui of the news feed

--- components/News.js
+++ components/News.js
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
 const News = ({ article }) => {
-  return <div>News</div>;
+  return (
+    <a href={article.url} target="_blank" rel="noreferrer">
+      <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200">
+        <div className="space-y-0.5">
+          <h6 className="text-sm font-bold">{article.title}</h6>
+          <p className="text-xs font-medium text-gray-500">
+            {article.source.name}
+          </p>
+        </div>
+        <img
+          src={article.urlToImage}
+          alt={article.title}
+          width={70}
+          className="rounded-xl "
+        />
+      </div>
+    </a>
+  );
 };
 
 export default News;
--- components/Widgets.js
+++ components/Widgets.js
@@ -15,9 +15,15 @@ const Widgets = ({ newsResult }) => {
           />
         </div>
       </div>
-      {newsResult.map((article) => (
-        <News key={article.title} article={article} />
-      ))}
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">What&apos;s hapenning</h4>
+        {newsResult.map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400">
+          Show more
+        </button>
+      </div>
     </div>
   );
 };
