4190bc5e3780e86f52bb8b8fbfe7dafcdca6224d
NwaguChima <nwaguchima48@gmail.com>
2022-06-24 17:32:29 +0100

    displaying comments

--- components/Comment.js
+++ components/Comment.js
@@ -1,5 +1,167 @@
-const Comment = ({ comment, id }) => {
-  return <div>{comment.comment}</div>;
+import { useEffect, useState } from "react";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  setDoc,
+  doc,
+  onSnapshot,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
+import React from "react";
+import Moment from "react-moment";
+import { db, storage } from "../firebase";
+import { signIn, useSession } from "next-auth/react";
+import { deleteObject, ref } from "firebase/storage";
+import { useRecoilState } from "recoil";
+import { modalState, postIdState } from "../atom/modalAtom";
+import { useRouter } from "next/router";
+
+const Post = ({ comment, commentId, originalPostId }) => {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", originalPostId, "comments", commentId, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db, originalPostId, commentId]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
+  }, [likes]);
+
+  const likeComment = async () => {
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(
+          doc(
+            db,
+            "posts",
+            originalPostId,
+            "comments",
+            commentId,
+            "likes",
+            session?.user.uid
+          )
+        );
+      } else {
+        await setDoc(
+          doc(
+            db,
+            "posts",
+            originalPostId,
+            "comments",
+            commentId,
+            "likes",
+            session?.user.uid
+          ),
+          {
+            username: session.user.username,
+          }
+        );
+      }
+    } else {
+      signIn();
+    }
+  };
+
+  const deleteComment = async () => {
+    if (window.confirm("Are you sure you want to delete this comment?")) {
+      await deleteDoc(doc(db, "posts", originalPostId, "comments", commentId));
+    }
+  };
+
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200 items-start pl-20">
+      <img
+        src={comment?.userImg}
+        alt="user-badge"
+        width={50}
+        height={50}
+        className="h-11 w-11 rounded-full mr-4"
+      />
+
+      <div className="flex-1">
+        <div className="flex items-center justify-between">
+          {/* post user info */}
+          <div className="flex space-x-1 items-center whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {comment?.name}
+            </h4>
+            <span className="text-sm sm:text-[13px]">
+              @{comment?.username} -{" "}
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              <Moment fromNow>{comment?.timestamp?.toDate()}</Moment>
+            </span>
+          </div>
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+          {comment?.comment}
+        </p>
+
+        <div className="flex justify-between text-gray-500 p-2">
+          <div className="flex items-center select-none">
+            <ChatIcon
+              onClick={() => {
+                if (!session) {
+                  signIn();
+                } else {
+                  setPostId(originalPostId);
+                  setOpen(!open);
+                }
+              }}
+              className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
+            />
+          </div>
+          {session?.user?.uid === comment?.userId && (
+            <TrashIcon
+              onClick={deleteComment}
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            />
+          )}
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likeComment}
+                className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likeComment}
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`${hasLiked && "text-red-600"} text-sm select-none`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+          <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Comment;
+export default Post;
--- components/CommentModal.js
+++ components/CommentModal.js
@@ -43,6 +43,7 @@ const CommentModal = () => {
       username: session.user.username,
       userImg: session.user.image,
       timestamp: serverTimestamp(),
+      userId: session.user.uid,
     });
 
     setOpen(false);
