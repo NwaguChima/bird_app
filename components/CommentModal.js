3b381196a337fb6b7a5847deb2829907de597ff1
NwaguChima <nwaguchima48@gmail.com>
2022-06-24 12:47:09 +0100

    setup the modal close functionality

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,6 +1,7 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
 import Modal from "react-modal";
+import { XIcon } from "@heroicons/react/outline";
 
 const CommentModal = () => {
   const [open, setOpen] = useRecoilState(modalState);
@@ -8,8 +9,21 @@ const CommentModal = () => {
   return (
     <div>
       {open && (
-        <Modal isOpen={open}>
-          <h1>Comment Modal</h1>
+        <Modal
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg h-[300px] w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white outline-none border-2 border-gray-200 rounded-xl shadow-md"
+        >
+          <div className="p-1">
+            <div className="border-b border-gray-200 py-2 px-1.5">
+              <div
+                onClick={() => setOpen(false)}
+                className="hoverEffect w-9 h-9 flex items-center justify-center"
+              >
+                <XIcon className="h-[22px] text-gray-700" />
+              </div>
+            </div>
+          </div>
         </Modal>
       )}
     </div>
