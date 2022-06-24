21cc151d22af5aaf7c0f4953087353fd30e9b78b
NwaguChima <nwaguchima48@gmail.com>
2022-06-24 17:35:04 +0100

    complete the comment component

--- components/Post.js
+++ components/Post.js
@@ -103,11 +103,15 @@ const Post = ({ post, id }) => {
           </div>
           <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
         </div>
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+        <p
+          onClick={() => router.push(`/posts/${id}`)}
+          className="text-gray-800 text-[15px] sm:text-[16px] mb-2"
+        >
           {post?.data()?.text}
         </p>
         {post?.data()?.image && (
           <img
+            onClick={() => router.push(`/posts/${id}`)}
             src={post?.data()?.image}
             alt="post-image"
             width={500}
